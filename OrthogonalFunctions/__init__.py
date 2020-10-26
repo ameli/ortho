ab11d4041bfa0bcb7aad20df1efb351c4a009f30
Siavash Ameli <sia.sunrise@gmail.com>
2020-10-25 21:31:43 -0700

    fix python 2 issue

--- OrthogonalFunctions/__init__.py
+++ OrthogonalFunctions/__init__.py
@@ -1,3 +1,12 @@
-from OrthogonalFunctions.ParseArguments import ParseArguments
-from OrthogonalFunctions.OrthogonalFunctions import OrthogonalFunctions
+# Depending on python 2 or 3, import relative to the directory or the package
+import sys
+if sys.version_info[0] == 2:
+    # For python 2
+    from .ParseArguments import ParseArguments
+    from .OrthogonalFunctions import OrthogonalFunctions
+else:
+    # For python 3
+    from OrthogonalFunctions.ParseArguments import ParseArguments
+    from OrthogonalFunctions.OrthogonalFunctions import OrthogonalFunctions
+
 from .__version__ import __version__
--- OrthogonalFunctions/__main__.py
+++ OrthogonalFunctions/__main__.py
@@ -5,8 +5,16 @@
 # =======
 
 import sys
-from OrthogonalFunctions import ParseArguments
-from OrthogonalFunctions import OrthogonalFunctions
+
+# Depending on python 2 or 3, import relative to the directory or the package
+if sys.version_info[0] == 2:
+    # For python 2
+    from ParseArguments import ParseArguments
+    from OrthogonalFunctions import OrthogonalFunctions
+else:
+    # Python 3
+    from OrthogonalFunctions import ParseArguments
+    from OrthogonalFunctions import OrthogonalFunctions
 
 # ====
 # Main
