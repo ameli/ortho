44005454ecceb12e727ff71da83b2928ad6a426e
Siavash Ameli <sia.sunrise@gmail.com>
2022-09-16 14:55:38 -0700

    add support for python 3.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 
 import os
 import sys
+from datetime import date
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -74,8 +75,8 @@ for build_subdirectory in build_subdirectories:
 # -- Project information -----------------------------------------------------
 
 project = 'ortho'
-copyright = '2022, Siavash Ameli'
 author = 'Siavash Ameli'
+copyright = f'{date.today().year}, ' + author
 
 
 # -- General configuration ---------------------------------------------------
