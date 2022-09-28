7ee266413442a6acfb3350a04fa09fb8742d81ad
Siavash Ameli <sia.sunrise@gmail.com>
2022-09-27 22:53:21 -0700

    update doc

---
+++ docs/source/cite.rst
@@ -0,0 +1,66 @@
+.. _cite:
+
+Citation
+========
+
+.. [1] Ameli, S., and Shadden. S. C. (2022). *Interpolating Log-Determinant and Trace of the Powers of Matrix* :math:`\mathbf{A} + t \mathbf{B}`. |ameli-int| |btn-bib-int| |btn-view-pdf-int|
+
+   .. raw:: html
+
+        <div class="highlight-BibTeX notranslate collapse" id="collapse-bib-int">
+        <div class="highlight">
+        <pre class="language-bib">
+        <code class="language-bib">@misc{arxiv.2009.07385,
+            doi = {10.48550/ARXIV.2009.07385},
+            author = {Ameli, S. and Shadden, S. C.}, 
+            title = {Interpolating Log-Determinant and Trace of the Powers of Matrix $\mathbf{A} + t \mathbf{B}$},
+            year = {2020}, 
+            archivePrefix={arXiv},
+            eprint = {2009.07385},
+            primaryClass={math.NA},
+            howpublished={\emph{arXiv}: 2009.07385 [math.NA]},
+        }</code></pre>
+        </div>
+        </div>
+        
+.. [2] Ameli, S. (2022). ameli/ortho: (v0.2.0). Zenodo. |ortho-zenodo| |btn-bib-ortho|
+
+   .. raw:: html
+
+        <div class="highlight-BibTeX notranslate collapse" id="collapse-bib-ortho">
+        <div class="highlight">
+        <pre class="language-bib">
+        <code class="language-bib">@misc{zenodo.308965310,
+            title = {ameli/ortho: (v0.2.0)},
+            author = {Ameli, S.},
+            year = {2022},
+            howpublished = {\url{https://pypi.org/project/ortho/}}
+        }</code></pre>
+        </div>
+        </div>
+
+.. |btn-bib-int| raw:: html
+
+    <button class="btn btn-outline-info btn-sm btn-extra-sm" type="button" data-toggle="collapse" data-target="#collapse-bib-int">
+        BibTeX
+    </button>
+
+.. |btn-bib-ortho| raw:: html
+
+    <button class="btn btn-outline-info btn-sm btn-extra-sm" type="button" data-toggle="collapse" data-target="#collapse-bib-ortho">
+        BibTeX
+    </button>
+    
+.. |btn-view-pdf-int| raw:: html
+
+    <button class="btn btn-outline-info btn-sm btn-extra-sm" type="button" id="showPDF-int">
+        View Article
+    </button>
+    
+.. |ameli-int| image:: https://img.shields.io/badge/arXiv-2009.07385-b31b1b.svg
+   :target: https://doi.org/10.48550/arXiv.2009.07385
+   :alt: arXiv 2009.07385
+
+.. |ortho-zenodo| image:: https://zenodo.org/badge/DOI/10.5281/zenodo.6395268.svg
+   :target: https://doi.org/10.5281/zenodo.6395268
+   :alt: doi: 10.5281/zenodo.6395268
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,6 +78,43 @@ project = 'ortho'
 author = 'Siavash Ameli'
 copyright = f'{date.today().year}, ' + author
 
+# -- Sphinx Settings ----------------------------------------------------------
+
+# Check links and references
+nitpicky = True
+
+# Why setting "root_doc": by using toctree in index.rst, two things are added
+# to index.html main page: (1) a toc in that location of page, (2) toc in the
+# sidebar menu. If we add :hidden: option to toctree, it removes toc from
+# both page and sidebar menu. There is no way we can have only one of these,
+# for instance, toc only in the page, but not in the menu. A solution to 
+# this is as follows:
+#   1. Set "root_doc= 'content'". Then add those toc that should go into the
+#      menu in the content.rst file.
+#   2. Add those toc that should go into the page in index.rst file.
+# This way, we can control which toc appears where.
+#
+# A problem: by setting "root_doc='content'", the sidebar logo links to
+# contents.html page, not the main page. There is a logo_url variable but it
+# does not seem to do anything. To fix this, I added a javascript (see in
+# /docs/source/_static/js/custom-pydata.js) which overwrites
+# <a href"path/contents.html"> to <a href="path/index.html>".
+# root_doc = "contents"
+
+# Common definitions for the whole pages
+rst_epilog = f'''
+.. role:: synco
+   :class: synco
+
+.. |project| replace:: :synco:`ortho`
+'''
+
+# Figure, Tables, etc numbering
+# numfig = True
+# numfig_format = {
+#     'figure': 'Figure %s',
+#     'table': 'Table %s'
+# }
 
 # -- General configuration ---------------------------------------------------
 
@@ -89,18 +126,28 @@ extensions = [
     'sphinx_math_dollar',
     'sphinx.ext.mathjax',
     'sphinx.ext.graphviz', 'sphinx.ext.inheritance_diagram',
-    'sphinx.ext.viewcode',
+    # 'sphinx.ext.viewcode',
     'sphinx_toggleprompt',
     # 'sphinx.ext.autosectionlabel',
     'sphinx.ext.autosummary',
     # 'sphinx_automodapi.automodapi',
-    # 'sphinxcontrib.napoleon'                # either use napoleon or numpydoc
-    'numpydoc'                                # either use napoleon or numpydoc
+    # 'sphinxcontrib.napoleon',               # either use napoleon or numpydoc
+    'numpydoc',                               # either use napoleon or numpydoc
+    'sphinx_design',
+    'sphinx-prompt',
+    'sphinx_copybutton',
 ]
+
+# Copy button settings
+copybutton_prompt_is_regexp = True
+copybutton_prompt_text = r'>>> |\.\.\. '
 
 # Automatically generate autosummary after each build
 autosummary_generate = True
 autosummary_imported_members = True
+
+# Remove the module names from the signature
+# add_module_names = False
 
 # automodapi
 numpydoc_show_class_members = False
@@ -152,19 +199,58 @@ html_theme = 'pydata_sphinx_theme'
 # Options for theme
 html_theme_options = {
     "github_url": "https://github.com/ameli/ortho",
-    "navbar_end": ["search-field.html", "navbar-icon-links.html"],
-    "page_sidebar_items": ["page-toc", "edit-this-page"]
+    "navbar_end": [
+        "theme-switcher",
+        "search-field.html",
+        "navbar-icon-links.html"
+    ],
+    "page_sidebar_items": ["page-toc", "edit-this-page"],
+    # "header_links_before_dropdown": 4,
+    "use_edit_page_button": True,
+    "icon_links": [
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/ortho/",
+            "icon": "fab fa-python",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Anaconda Cloud",
+            "url": "https://anaconda.org/s-ameli/ortho",
+            "icon": "fa fa-circle-notch",
+            "type": "fontawesome",
+        },
+    ],
+    "pygment_light_style": "tango",
+    "pygment_dark_style": "native",
+    "logo": {
+        "image_light": "images/icons/logo-ortho-light.png",
+        "image_dark": "images/icons/logo-ortho-dark.png",
+    },
+}
+
+html_context = {
+    "default_mode": "auto",
+    "github_url": "https://github.com",
+    "github_user": "ameli",
+    "github_repo": "glearn",
+    "github_version": "main",
+    "doc_path": "docs/source",
 }
 
 html_sidebars = {
     "**": ["sidebar-nav-bs", "sidebar-ethical-ads"]
 }
 
-project = u'ortho'
-copyright = u'2022, Siavash Ameli'
+# Using Font Awesome icons
+# html_css_files = [
+#     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+# ]
+
 
 html_title = f"{project} Manual"
 html_last_updated_fmt = '%b %d, %Y'
+# html_show_sourcelink = False
 
 # To use sphinx3, download at
 # https://github.com/sphinx-doc/sphinx/tree/master/doc/_themes/sphinx13
@@ -194,10 +280,12 @@ html_last_updated_fmt = '%b %d, %Y'
 html_static_path = ['_static']
 
 # # Add css
-# html_css_files = [
-#     'custom.css',
-# ]
+# html_css_files = ["css/custom.css"]
+# html_css_files = ['css/custom-anaconda-doc.css']
 
+html_js_files = ["js/custom-pydata.css"]
+# html_logo = '_static/images/icons/logo-imate-light.png'
+html_favicon = '_static/images/icons/favicon.ico'
 
 # =====
 # setup
@@ -206,8 +294,10 @@ html_static_path = ['_static']
 def setup(app):
     """
     This function is used to employ a css file to the themes.
-    Note: paths are relative to /docs/_static
+    Note: paths are relative to /docs/source/_static
     """
 
-    app.add_css_file('css/custom.css')
+    app.add_css_file('css/custom-pydata.css')
+    app.add_js_file('js/custom-pydata.js')
+    # app.add_css_file('css/custom.css')
     # app.add_css_file('css/custom-anaconda-doc.css')
